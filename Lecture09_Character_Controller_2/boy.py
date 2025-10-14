7f9af215977621378cabedfda4de5f157c3da32b
Cho Kisung <kisung128@gmail.com>
2025-10-14 11:15:07 +0900

    Initial commit

--- Lecture09_Character_Controller_2/boy.py
+++ Lecture09_Character_Controller_2/boy.py
@@ -3,9 +3,34 @@ from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT
 
 from state_machine import StateMachine
 
+def space_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_SPACE
+
+def time_out(e):
+    return e[0] == 'TIME_OUT'
+
+class Sleep:
+    def __init__(self, boy):
+        self.boy = boy
+
+    def enter(self):
+        self.boy.dir = 0
+
+    def exit(self):
+        pass
+
+    def do(self):
+        self.boy.frame = (self.boy.frame + 1) % 8
+
+    def draw(self):
+        if self.boy.face_dir == 1: # right
+            self.boy.image.clip_composite_draw(self.boy.frame * 100, 300, 100, 100, 3.141592 / 2, "",
+                                               self.boy.x - 25, self.boy.y - 25, 100, 100)
+        else: # face_dir == -1: # left
+            self.boy.image.clip_composite_draw(self.boy.frame * 100, 200, 100, 100, -3.141592/2,"",
+                                               self.boy.x + 25, self.boy.y - 25, 100, 100)
 
 class Idle:
-
     def __init__(self, boy):
         self.boy = boy
 
@@ -24,7 +49,6 @@ class Idle:
         else: # face_dir == -1: # left
             self.boy.image.clip_draw(self.boy.frame * 100, 200, 100, 100, self.boy.x, self.boy.y)
 
-
 class Boy:
     def __init__(self):
         self.x, self.y = 400, 90
@@ -34,11 +58,20 @@ class Boy:
         self.image = load_image('animation_sheet.png')
 
         self.IDLE = Idle(self)
-        self.state_machine = StateMachine(self.IDLE)
+        self.SLEEP = Sleep(self)
+        self.state_machine = StateMachine(
+            self.SLEEP,
+            {
+                self.SLEEP : {space_down, self.IDLE},
+                self.IDLE : {time_out, self.IDLE}
+            }
+        )
 
     def update(self):
         self.state_machine.update()
 
+    def handle_event(self, event):
+        self.state_machine.handle_event(('INPUT', event))
 
     def draw(self):
         self.state_machine.draw()
--- Lecture09_Character_Controller_2/state_machine.py
+++ Lecture09_Character_Controller_2/state_machine.py
@@ -1,12 +1,16 @@
 from event_to_string import event_to_string
 
 class StateMachine:
-    def __init__(self, start_state):
+    def __init__(self, start_state, rules):
         self.cur_state = start_state
+        self.rules = rules
 
     def update(self):
         self.cur_state.do()
 
     def draw(self):
         self.cur_state.draw()
+
+    def handle_state_event(self, state_event):
+
 
