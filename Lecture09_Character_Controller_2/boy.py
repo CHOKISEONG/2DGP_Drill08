1f90b212e499db8ce6be1d6f3230379c4d2c862d
Cho Kisung <kisung128@gmail.com>
2025-10-15 16:15:51 +0900

    AutoRun 상태일 때 키운 크기를 지형에 맞게 enter,exit 설정

--- Lecture09_Character_Controller_2/boy.py
+++ Lecture09_Character_Controller_2/boy.py
@@ -28,20 +28,24 @@ class AutoRun:
 
     def enter(self, e):
         self.boy.wait_start_time = get_time()
+        self.boy.y = 130
 
     def exit(self, e):
+        self.boy.y = 90
         pass
 
     def do(self):
         self.boy.frame = (self.boy.frame + 1) % 8
-        if get_time() - self.boy.wait_start_time >= 5.0: # Idle 상태에서 2초 경과
+
+        if get_time() - self.boy.wait_start_time >= 5.0:
             self.boy.state_machine.handle_state_event(('TIME_OUT', None))
+
 
     def draw(self):
         if self.boy.face_dir == 1: # right
-            self.boy.image.clip_draw(self.boy.frame * 100, 300, 100, 100, self.boy.x, self.boy.y, 200, 200)
+            self.boy.image.clip_draw(self.boy.frame * 100, 100, 100, 100, self.boy.x, self.boy.y, 200, 200)
         else: # face_dir == -1: # left
-            self.boy.image.clip_draw(self.boy.frame * 100, 200, 100, 100, self.boy.x, self.boy.y, 200, 200)
+            self.boy.image.clip_draw(self.boy.frame * 100, 0, 100, 100, self.boy.x, self.boy.y, 200, 200)
 
 
 class Run:
